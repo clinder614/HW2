b735c50cb8a00939c6b05547ef86b7c5f634779b
clinder614 <clinder614@icloud.com>
2016-09-22 11:31:40 -0600

    Update Mathematica.java

--- Mathematica.java
+++ Mathematica.java
@@ -7,11 +7,23 @@ package hw2;
 
 /**
  *
- * @author courtneylinder
+ * @author courtneylinder, kierstyn brandt
  */
 public class Mathematica extends MathProgram{
     public Mathematica(){
-    
+        b = new Insertion();
+        
+    }
+    public void performSort(){
+        System.out.println("Unsorted Array");
+        for(int i = 0; i < 10; i++){
+            System.out.print(array[i] + " ");
+        }
+        b.mathSort(array);
+        System.out.println("\nSorted Array");
+        for(int i = 0; i < 10; i++){
+            System.out.print(array[i] + " ");
+        }
     }
     
 }
