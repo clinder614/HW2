bd2f00356a9c1bbf983f8b44aa729c7fc87608df
clinder614 <clinder614@icloud.com>
2016-09-22 11:52:09 -0600

    Update MathProgram.java

--- MathProgram.java
+++ MathProgram.java
@@ -5,18 +5,23 @@
  */
 package hw2;
 
+
 /**
  *
- * @author courtneylinder
+ * @author courtneylinder, kierstyn brandt
  */
 public class MathProgram {
     MathBehavior b;
+    int[] array = new int[10]; 
+    
     public MathProgram(){
-        
+       for(int i = 0; i < 10; i++){
+            array[i] = (int) (Math.random() * 20);
+        }    
     }
     
     public void performSort(){
-    
+        b.mathSort(array);
     }
     
     public void setMathBehavior(MathBehavior b){
