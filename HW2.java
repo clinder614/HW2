8382238d9586f7ef3a0ec405f102902b2746df39
clinder614 <clinder614@icloud.com>
2016-09-22 10:33:31 -0600

    Update HW2.java

--- HW2.java
+++ HW2.java
@@ -5,6 +5,8 @@
  */
 package hw2;
 
+import java.util.Scanner;
+
 /**
  *
  * @author courtneylinder
@@ -15,8 +17,32 @@ public class HW2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        MathProgram mp = new MathProgram();
-        mp.pickProgram();
+        Scanner userInput = new Scanner( System.in );
+        int input;
+        MathProgram mp;
+        boolean okay = false;
+        while(!okay){
+            System.out.println("What math program would you like to use?");
+            System.out.println("Press 1 for MyMath, 2 for MTool, and 3 for Mathmatica.");
+            input = userInput.nextInt();
+            if(input == 1){
+                mp = new MyMath();
+                okay = true;
+            }
+            else if(input == 2){
+                mp = new MTool();
+                okay = true;
+            }
+            else if(input == 3){
+                mp = new Mathematica();
+                okay = true;
+            }
+            else{
+                System.out.println("You do not enter a valid choice. Please try again.");
+            }
+        
+        }
+        
     }
     
 }
