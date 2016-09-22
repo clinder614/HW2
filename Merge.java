31d074388c9de84255750947de9fd5cbabb58d80
kmbrandt95 <musikalkmbrandt@gmail.com>
2016-09-22 10:45:10 -0600

    Create Merge.java

---
+++ Merge.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package hw2;
+
+/**
+ *
+ * @author courtneylinder, kierstynbrandt
+ */
+public class Merge implements MathBehavior {
+    
+     public void mathSort(int[] num){
+        
+     }
+    
+}
