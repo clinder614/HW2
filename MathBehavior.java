859824cc0c7a0d8f3fc6dea543d53c5c2717477a
kmbrandt95 <musikalkmbrandt@gmail.com>
2016-09-22 10:44:06 -0600

    Create MathBehavior.java

---
+++ MathBehavior.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package hw2;
+
+/**
+ *
+ * @author courtneylinder, kierstynbrandt
+ */
+ interface MathBehavior {
+   void mathSort(int[] num);
+}
