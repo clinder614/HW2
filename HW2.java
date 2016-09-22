e269a4137f121eebc31ab5640ea459f981bcaaff
clinder614 <clinder614@icloud.com>
2016-09-22 10:06:36 -0600

    Create HW2.java

---
+++ HW2.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hw2;
+
+/**
+ *
+ * @author courtneylinder
+ */
+public class HW2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        MathProgram mp = new MathProgram();
+        mp.pickProgram();
+    }
+    
+}
